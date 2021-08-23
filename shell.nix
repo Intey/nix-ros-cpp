86a9ad9ce9a0e5a127f96f90914e5148e813e809
intey <ziexe0@gmail.com>
2021-08-23 12:13:14 +0300

    c++20 & clang

--- shell.nix
+++ shell.nix
@@ -1,3 +1,11 @@
+let pkgs = import (builtins.fetchTarball {
+  name = "nixos-21.20";
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
+  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+}) {};
+
+in 
+
 with import (builtins.fetchTarball https://github.com/lopsided98/nix-ros-overlay/archive/master.tar.gz) {};
 
 # when we try `with rosPackages` and specify dist in paths, we get error:
@@ -6,10 +14,15 @@ with import (builtins.fetchTarball https://github.com/lopsided98/nix-ros-overlay
 with rosPackages.noetic;
 
 mkShell {
-  buildInputs = [
+  buildInputs = with pkgs; [
+    clang_12
+    ninja
     gtest
     cmake
-    ninja
+    gdb
+    llvmPackages_12.libstdcxxClang
+    libstdcxx5
+
     (buildEnv { 
       name = "ros";
       paths = [
--- src/listener/main.cpp
+++ src/listener/main.cpp
@@ -1,6 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <concepts>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
+template<class T>
+concept IterToComparable = requires(T a, T b)
+{
+  {
+    *a < *b
+  }
+  ->std::convertible_to<bool>;
+};
+
+// обратите внимание на IterToComparable вместо слова class
+template<IterToComparable InputIt>
+void
+SortDefaultComparator(InputIt begin, InputIt end)
+{
+  std::sort(begin, end);
+}
+
+struct X
+{
+  auto operator<=>(const X&) const = default;
+  int a;
+};
+
+int
+concept_main()
+{
+  std::vector<X> v = { { 10 }, { 9 }, { 11 } };
+  SortDefaultComparator(v.begin(), v.end());
+  return 0;
+}
+
+/*
 void
 chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
@@ -8,9 +43,12 @@ chatterCallback(const std_msgs::String::ConstPtr& msg)
   ROS_INFO("There something on the line! [%s]", msg->data.c_str());
 }
 
+*/
 int
 main(int argc, char** argv)
 {
+  concept_main();
+/*
   //Создаем узел listener
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
@@ -21,5 +59,7 @@ main(int argc, char** argv)
   // прекратит работу
   ros::spin();
 
+*/
+  std::cout << "done" << std::endl;
   return 0;
 }
