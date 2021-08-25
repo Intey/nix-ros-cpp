292ee9d7d18c9ad69307819d14c5a48058d4289f
Intey <ziexe0@gmail.com>
2021-08-25 13:36:20 +0300

    add UE4. Can't build because broken cdn

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 let pkgs = import (builtins.fetchTarball {
-  name = "nixos-21.20";
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
-  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
-}) {};
-
+      name = "nixos-21.20";
+      url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
+      sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+    }) {};
+    ue4_4_27 = pkgs.ue4.override { requireFile = unused_args: pkgs.fetchFromGitHub { 
+        owner = "EpicGames";
+        repo = "UnrealEngine";
+        rev = "4.27.0-release";
+        sha256 = "0b43239621fe8d7d536b02be79e17a41036805bf0426ebb98f3ec93d17c2c3e9";
+    }; };
 in 
 
 with import (builtins.fetchTarball https://github.com/lopsided98/nix-ros-overlay/archive/master.tar.gz) {};
@@ -16,13 +21,11 @@ with rosPackages.noetic;
 mkShell {
   buildInputs = with pkgs; [
     clang_12
+    ue4_4_27
     ninja
     gtest
     cmake
     gdb
-    llvmPackages_12.libstdcxxClang
-    libstdcxx5
-
     (buildEnv { 
       name = "ros";
       paths = [
